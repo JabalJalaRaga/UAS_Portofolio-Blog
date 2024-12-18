c0538e39da79ad71bacdca223e464f175fd13746
JabalJalaRaga <elizabetfiona@gmail.com>
2024-12-18 20:59:19 +0700

    perbaikan login

--- src/components/PublicMenu.js
+++ src/components/PublicMenu.js
@@ -9,29 +9,26 @@ export default function PublicMenu() {
     }
 
     return (
-        <header className="py-4 bg-gradient-to-r from-blue-500 to-teal-500 shadow-lg">
+        <header className="py-4 bg-gradient-to-r from-teal-400 to-green-500 shadow-2xl">
             <nav className="w-full">
                 <div className="max-w-5xl mx-auto px-6 md:px-12 xl:px-6">
                     <div className="flex flex-wrap items-center justify-between">
                         <div>
-                            <a href="/" className="text-3xl font-bold text-white hover:text-gray-200 transition duration-300">
+                            <a href="/" className="text-1xl font-bold text-gray-100 hover:text-gray-200 transition duration-300">
                                 Jabal Jala Raga
                             </a>
                         </div>
                         <div>
                             <ul className="flex flex-row gap-6">
-                                {/* <li className="mt-1">
-                                    <a className="btn-link" href="/about">About</a>
-                                </li> */}
                                 <li className="mt-1">
-                                    <a className="btn-link text-white hover:text-gray-200 transition duration-300" href="/blogs">Blogs</a>
+                                    <a className="btn-link text-gray-200 hover:text-gray-300 transition duration-300" href="/blogs">Blogs</a>
                                 </li>
                                 <li className="mt-1">
-                                    <a className="btn-link text-white hover:text-gray-200 transition duration-300" href="/contact">Contact</a>
+                                    <a className="btn-link text-gray-200 hover:text-gray-300 transition duration-300" href="/contact">Contact</a>
                                 </li>
                                 <li>
                                     <button
-                                        className="btn-primary bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded-lg transition duration-300"
+                                        className="btn-primary bg-green-600 hover:bg-green-700 text-white py-2 px-4 rounded-lg shadow-lg transition duration-300"
                                         onClick={onLoginPage}
                                     >
                                         <span className="relative text-sm font-semibold">
